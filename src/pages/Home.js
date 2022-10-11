ce9feeb67ebc2c8294ed7a8ff6ef527b256ad4e6
Zahra Sherinpour <sherin99zahra@gmail.com>
2022-10-11 09:35:14 +0330

    complete css of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Helmet from "../components/helmet/Helmet";
 import ProductsList from "../components/UI/productsList/ProductsList";
-import heroImg from "../assets/images/hero-img.png";
+import heroImg from "../assets/images/hero-img.jpg";
 import products from "../assets/data/products";
 import "../styles/home.css";
 
@@ -25,30 +25,58 @@ const Home = () => {
 
   return (
     <Helmet title="Home">
-      <section className="hero_section">
-        <div className="container">
-          <div className="hero_content">
-            <h2>Title</h2>
-            <p>make ahglhf jWHFJH JJESHFHLS AIHEUIF</p>
-            <button className="by_btn">
-              <Link to="/shop">SHOP NOW</Link>
-            </button>
-          </div>
-          <div className="hero_img">
-            <img src={heroImg} alt="" />
-          </div>
+      <div className="banner">
+        <div className="banner-desc">
+          <h1>
+            Find your dream watch on the leading marketplace for luxury watches.
+          </h1>
+          <p>
+            Buying eyewear should leave you happy and good-looking, with money
+            in your pocket. Glasses, sunglasses, and contacts—we’ve got your
+            eyes covered.
+          </p>
+
+          <Link class="button" to="/shop">
+            Shop Now &nbsp;
+            <span
+              role="img"
+              aria-label="arrow-right"
+              class="anticon anticon-arrow-right"
+            >
+              <svg
+                viewBox="64 64 896 896"
+                focusable="false"
+                data-icon="arrow-right"
+                width="1em"
+                height="1em"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 000-48.4z"></path>
+              </svg>
+            </span>
+          </Link>
         </div>
-      </section>
+        <div className="banner-img">
+          <img src={heroImg} alt="" />
+        </div>
+      </div>
 
-      <section className="trending_products">
-        <h2> Trending Products </h2>
+      <div className="display">
+        <div className="display-header">
+          <h1>Featured Products</h1>
+          <Link to="/shop">See All</Link>
+        </div>
         <ProductsList data={trendingProducts} />
-      </section>
+      </div>
 
-      <section className="new_arivals">
-        <h2>Watches </h2>
+      <div className="display">
+        <div className="display-header">
+          <h1>Recommended Products</h1>
+          <Link to="/shop">See All</Link>
+        </div>
         <ProductsList data={watchProducts} />
-      </section>
+      </div>
     </Helmet>
   );
 };
